24c91eeee505c14eb458983de599fc7e8ab59330
Michoel Samuels <k2co3@icloud.com>
2017-08-15 13:45:21 +0300

    Shuffle test run order

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,17 +8,19 @@
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require "minitest/autorun"
-require 'minitest/color'
-
-require 'audio_file_joiner'
-require 'file_selector'
-require 'text_to_speech_toolkit'
-
 # Coverage reporter
 require 'simplecov'
 SimpleCov.start
 
 # Enable Coveralls test coverage tester
 require 'coveralls'
-Coveralls.wear!
+# Coveralls.wear!
+
+require "minitest/autorun"
+require 'minitest/color'
+
+
+require 'audio_file_joiner'
+require 'file_selector'
+require 'text_to_speech_toolkit'
+
